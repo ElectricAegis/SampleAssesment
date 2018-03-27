fc2f621fbbb589f6f3ee146d02c23746ecc939f8
Manie <manie.brummer@gmail.com>
2018-03-27 15:34:15 +0200

    ?

--- BlackJack21App/BlackJackEvaluator.cs
+++ BlackJack21App/BlackJackEvaluator.cs
@@ -1,13 +1,145 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Media;
 
 namespace BlackJack21App
 {
     public class BlackJackEvaluator
     {
+
+        private Dictionary<string, int> _cardSymbols = new Dictionary<string, int>()
+        {
+            {"Ace", 1 },
+            {"Two", 2 },
+            {"Three", 3 },
+            {"Four", 4 },
+            {"Five", 5 },
+            {"Six", 6 },
+            {"Seven", 7 },
+            {"Eight", 8 },
+            {"Nine", 9 },
+            {"Ten", 10 },
+            {"King", 10 },
+            {"Queen", 10 },
+            {"Jack", 10 },
+        };
+
         public List<string> Evaluate(Dictionary<string, List<string>> playerHands)
         {
             throw new NotImplementedException();
         }
+
+        public List<string> PlayersWithHandsHigherThanDealerWin(Dictionary<string, List<string>> playerHands)
+        {
+            List<string> winners = new List<string>();
+            Dictionary<string, int> playerTotals = new Dictionary<string, int>();
+
+            var sum = 0;
+            var player = "";
+
+            foreach (var playerHand in playerHands)
+            {
+                var cardsInHand = playerHands[playerHand.Key];
+                foreach (var cardInHand in cardsInHand)
+                {
+                    foreach (var cardSymbol in this._cardSymbols.Keys)
+                    {
+                        if (cardInHand.Contains(cardSymbol))
+                        {
+                            player = playerHand.Key;
+                            sum += this._cardSymbols[cardSymbol];
+                            playerTotals.Add(player,sum);
+                        }
+                    }
+                }
+
+                Console.WriteLine("Player:{0} - Sum:{1} ", player, sum);
+                sum = 0;
+            }
+
+            foreach (var playerTotal in playerTotals)
+            {
+                var PlayerHandCount = playerHands[playerTotal.Key].Count;
+                var DealerHandCount = playerHands["Dealer"].Count;
+
+                if (PlayerHandCount > DealerHandCount)
+                {
+                    if (playerTotals[playerTotal.Key] > playerTotals["Dealer"])
+                        winners.Add(playerTotal.Key);
+                }
+            }
+
+
+            return winners;
+        }
+
+
+        public List<string> PlayerWithFiveCardsUnder21BeatsDealer(Dictionary<string, List<string>> playerHands)
+        {
+
+            List<string> winners = new List<string>();
+            Dictionary<string, int> playerTotals = new Dictionary<string, int>();
+
+            var sum = 0;
+            var player = "";
+
+            foreach (var playerHand in playerHands)
+            {
+                var cardsInHand = playerHands[playerHand.Key];
+                foreach (var cardInHand in cardsInHand)
+                {
+                    foreach (var cardSymbol in this._cardSymbols.Keys)
+                    {
+                        if (cardInHand.Contains(cardSymbol))
+                        {
+                            player = playerHand.Key;
+                            sum += this._cardSymbols[cardSymbol];
+                            playerTotals.Add(player, sum);
+                        }
+                    }
+                }
+
+                Console.WriteLine("Player:{0} - Sum:{1} ", player, sum);
+                if(playerHands[player].Count == 5 && playerTotals[player] < 21)
+                    winners.Add(player);
+
+                sum = 0;
+            }
+
+            return winners;
+        }
+
+        public List<string> PlayerWithAceChoosesNotToGoOver21(Dictionary<string, List<string>> playerHands)
+        {
+
+            List<string> winners = new List<string>();
+            Dictionary<string, int> playerTotals = new Dictionary<string, int>();
+
+            var sum = 0;
+            var player = "";
+
+            foreach (var playerHand in playerHands)
+            {
+                var cardsInHand = playerHands[playerHand.Key];
+                foreach (var cardInHand in cardsInHand)
+                {
+                    foreach (var cardSymbol in this._cardSymbols.Keys)
+                    {
+                        if (cardInHand.Contains(cardSymbol))
+                        {
+                            player = playerHand.Key;
+                            sum += this._cardSymbols[cardSymbol];
+                            playerTotals.Add(player, sum);
+                        }
+                    }
+                }
+                sum = 0;
+            }
+
+            return new List<string>();
+        }
+
+
+
     }
 }
--- BlackJack21App/Program.cs
+++ BlackJack21App/Program.cs
@@ -10,6 +10,22 @@ namespace BlackJack21App
     {
         static void Main(string[] args)
         {
+            var evaluator = new BlackJackEvaluator();
+            var playerHands = new Dictionary<string, List<string>>
+            {
+                {"Dealer",new List<string> {"Six of Clubs", "Nine of Clubs"} },
+                {"Billy",new List<string> {"Queen of Diamonds", "King of Clubs"} },
+                {"Andrew",new List<string> {"Nine of Hearts", "Six of Clubs", "Jack of Hearts"} },
+                {"Carla",new List<string> {"Two of Clubs", "Nine of Diamonds", "King of Clubs"} }
+            };
+
+            foreach(var player in evaluator.PlayersWithHandsHigherThanDealerWin(playerHands))
+            {
+                Console.WriteLine(player);
+            }
+
+            Console.Read();
+
         }
     }
 }
